3aba1a156cddeafc1547070ae6dbefe598ec3a2c
palvisha13 <60575398+palvisha13@users.noreply.github.com>
2020-07-20 17:47:18 -0400

    Dice roll thingy

---
+++ task4.js
@@ -0,0 +1,17 @@
+/* Task4.js - Add your Java Script Code Here */
+
+// function to roll the dice
+
+function roll_dice(){
+   return (Math.random()*5)+1;
+}
+var number_pair = [];
+function roll_twice(){
+    for (i=1;i<=2;i++){
+        number_pair = number_pair.push(roll_dice());
+    }
+
+    document.getElementById("mydata").innerHTML=number_pair.values();
+}
+    
+
